b11ce65288bd15ae89425621ee55402560f766dc
Guilherme Felix <guilhermedesoares@gmail.com>
2026-02-02 20:49:11 -0300

    feat: adiciona enums do domínio
    Criei enums para representar estados e tipos utilizados
    nas regras de negócio, evitando uso de valores mágicos no código
    e deixando as regras mais legíveis.

---
+++ backend/ControleGastos.Domain/Enums/EFinalidadeCategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleGastos.Domain.Enums
+{
+    public enum EFinalidadeCategoria
+    {
+        Receita = 1,
+        Despesa = 2,
+        Ambas = 3
+    }
+}
