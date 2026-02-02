e525905ce13a0335bc4e78ef67cb66105bc7ce24
Guilherme Felix <guilhermedesoares@gmail.com>
2026-02-02 20:43:21 -0300

    feat: estrutura inicial da API de controle de gastos
    Organizei o projeto em camadas (Domain, Application, Infrastructure e API),
    configurei EF Core, Swagger, DI e estrutura base para controllers e repositórios.

---
+++ backend/ControleGastos.Api/Program.cs
@@ -0,0 +1,75 @@
+using ControleGastos.Api.Middlewares;
+using ControleGastos.Application.Dtos;
+using ControleGastos.Application.Queries;
+using ControleGastos.Application.Service;
+using ControleGastos.Application.UseCase;
+using ControleGastos.Application.Validators;
+using ControleGastos.Domain.Interfaces;
+using ControleGastos.Infrastructure.Data;
+using ControleGastos.Infrastructure.Repositories;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(op =>
+op.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddValidatorsFromAssemblyContaining<PessoaValidator>();
+builder.Services.AddFluentValidationAutoValidation();
+
+// Service
+builder.Services.AddScoped<PessoaService>();
+builder.Services.AddScoped<CategoriaService>();
+builder.Services.AddScoped<TransacaoService>();
+
+// Repositorio
+builder.Services.AddScoped<IPessoaRepository, PessoaRepository>();
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<ITransacaoRepository, TransacaoRepository>();
+builder.Services.AddScoped<ISaldoQuery, SaldoQueryRepository>();
+
+// UseCase
+builder.Services.AddScoped<CriarTransacaoUseCase>();
+builder.Services.AddScoped<RelatorioTransacaoPorPessoaUseCase>();
+builder.Services.AddScoped<RelatorioTransacaoPorCategoriaUseCase>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = string.Empty;
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Controle de Gastos API v1");
+    });
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+
+app.Run();
+
+
+
+
+
